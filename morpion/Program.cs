f8faa554845cbc9eed2d12dd33fa9984c0ec74ed
Louis Funck-Brentano <louisfunckbrentano@gmail.com>
2015-06-17 16:14:55 +0200

    add new program (morpion)

---
+++ morpion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace morpion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cki = Console.ReadKey();
+
+            Console.WriteLine("#######");
+            Console.WriteLine("# # # #");
+            Console.WriteLine("#######");
+            Console.WriteLine("# # # #");
+            Console.WriteLine("#######");
+            Console.WriteLine("# # # #");
+            Console.WriteLine("#######");
+
+            
+
+
+        }
+    }
+}
